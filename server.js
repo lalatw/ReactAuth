bfc33e7961118422b691b6c424fc0a358eea8e41
L Lee <lauralee.sh@gmail.com>
2018-10-10 16:35:26 -0700

    update

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-// const routes = require("./routes");
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// require("./routes/api/auth.js")(app);
+require("./routes/api/auth.js")(app);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(express.static("client/build"));
 
-// app.use(routes);
+app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
